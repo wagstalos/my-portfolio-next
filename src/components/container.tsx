f839736959152a8f6a12c88a99551bd60f22f8d3
Wagner Paulo <wagstalos@hotmail.com>
2025-03-13 19:07:11 -0300

    erro : vercel

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,3 +1,9 @@
-export function ContainerGrid({ children }) {
+import { ReactNode } from "react";
+
+interface ContainerGridProps {
+  children: ReactNode; // Tipando o children como ReactNode
+}
+
+export function ContainerGrid({ children }: ContainerGridProps) {
   return <div className="max-w-[86rem] mx-auto">{children}</div>;
 }
