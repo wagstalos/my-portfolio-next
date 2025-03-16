e8da34a7f1b00b4841180a5e6c9e844f0dba30a9
Wagner Paulo <wagstalos@hotmail.com>
2025-03-16 09:26:43 -0300

    fix: bg: nav

--- src/components/navComponent.jsx
+++ src/components/navComponent.jsx
@@ -19,7 +19,7 @@ export default function Nav() {
   };
 
   return (
-    <nav className="text-white p-4 border-b border-slate-500/30 fixed top-0 left-0 w-full z-50 bg-[#0f1018]">
+    <nav className="text-white p-4 border-b border-slate-500/30 fixed top-0 left-0 w-full z-50 bg-[#0C0D15]">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold">
           <Image src={logo} alt="Wagner Silva" title="Wagner Silva" width={200} />
@@ -65,22 +65,22 @@ export default function Nav() {
       {/* Menu Mobile */}
       {isOpen && (
         <ul className="md:hidden flex flex-col items-center gap-4 p-4 bg-[#0C0D15]">
-          <li className="hover:text-amber-300">
+          <li className="hover:text-amber-300 p-2">
             <a href="#quemsou" onClick={(e) => scrollToSection(e, "#quemsou")}>
               Quem sou
             </a>
           </li>
-          <li className="hover:text-amber-300">
+          <li className="hover:text-amber-300 p-2">
             <a href="#meusprojetos" onClick={(e) => scrollToSection(e, "#meusprojetos")}>
               Projetos
             </a>
           </li>
-          <li className="hover:text-amber-300">
+          <li className="hover:text-amber-300 p-2">
             <a href="#experiencia" onClick={(e) => scrollToSection(e, "#experiencia")}>
               Experiência
             </a>
           </li>
-          <li className="hover:text-amber-300 mt-4">
+          <li className="hover:text-amber-300 p-2 mt-4">
             <a href="curriculo_wagner.pdf" download className="flex items-center border border-blue-100/20 bg-blue-200/10 text-white px-8 py-2 rounded-full font-semibold hover:border-amber-400 hover:text-amber-400 transition-all duration-300 ease-in-out">
               <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
